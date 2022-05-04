13d8444a176bff1ce049adcd8debeeb6b729a91d
Aleksey <kinderlolpingui@yandex.ru>
2022-05-04 07:07:57 +0300

    menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,29 +5,39 @@ new Swiper('.comment-slider', {
    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
+   },
 });
 
 new Swiper('.promo-slider', {
 
    loop: true,
-   
+
    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
+   },
 });
 
 new Swiper('.mini-slider', {
 
    loop: true,
-   
+
    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
+   },
 
-    direction: 'vertical',
-    slidesPerView: 3,
-    spaceBetween: 24,
+   direction: 'vertical',
+   slidesPerView: 3,
+   spaceBetween: 24,
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+   const menu = document.querySelector('.menu');
+   const hamburger = document.querySelector('.button--nav');
+
+   hamburger.addEventListener('click', () => {
+      menu.classList.toggle('menu--active');
+   });
+
+})
